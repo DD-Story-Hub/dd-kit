ca46987f82a5390c8b99737e802d95cae9e4f923
Steven Yeo <shihjyun.yeo@gmail.com>
2020-09-20 03:24:06 +0800

    - add logo page - add favicon - add static logo - change default setting

---
+++ catalog/DD-brand/brand-logo.md
@@ -0,0 +1,60 @@
+這邊提供各種尺寸的 DD 品牌識別工具，可直接對圖像點選右鍵儲存得到 SVG 檔。
+
+
+
+```image
+plain: true
+light: true
+span: 2
+src: "/logo/DD-avatar-text.svg"
+title: "品牌型 ( Logo + 中文 )"
+description: 適合當作 FB 頭像，也可以當作中文自製圖片/圖表的品牌識別圖。
+```
+```image
+plain: true
+light: true
+span: 2
+src: "/logo/DD-avatar-text-en.svg"
+title: "品牌型 ( Logo + 英文 )"
+description: 適合當作 FB 頭像，也可以當作英文自製圖片/圖表的品牌識別圖。
+```
+```image
+plain: true
+light: true
+span: 2
+src: "/logo/DD-avatar-gray-text.svg"
+title: "品牌灰階型 ( Logo + 中文 )"
+description: 同左中文，但灰色感覺酷酷的。
+```
+```image
+plain: true
+light: true
+span: 2
+src: "/logo/DD-avatar-gray-text-en.svg"
+title: "品牌灰階型 ( Logo + 英文 )"
+description: 同左英文，但灰色感覺酷酷的。
+```
+```image
+plain: true
+light: true
+span: 2
+src: "/logo/DDSH-avatar.svg"
+title: "簡約品牌型 ( Logo )"
+description: 無字純 Logo，簡約洗鍊，適合做各類型自製圖像、字卡、圖表的品牌識別。
+```
+```image
+plain: true
+light: true
+span: 2
+src: "/logo/DDSH-avatar-gray.svg"
+title: "簡約品牌灰階型 ( Logo )"
+description: 同左，但灰色感覺酷酷的。
+```
+```image
+plain: true
+light: true
+span: 3
+src: "/logo/DD-regular-logo-text.svg"
+title: "萬用型 ( Logo + 中文 + 英文 )"
+description: 圖如其名，貼在哪都沒問題。
+```
--- catalog/index.js
+++ catalog/index.js
@@ -1,16 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Catalog, pageLoader } from "catalog";
+import logo from "./static/logo/DD-regular-logo-text.svg";
 
 const pages = [
   {
     path: "/",
-    title: "Welcome",
-    content: pageLoader(() => import("./WELCOME.md"))
+    title: "認識 DD",
+    content: pageLoader(() => import("./DD-welcome.md"))
+  },
+  {
+    title: '品牌',
+    pages: [
+      {
+        path: '/DD-brand/logo',
+        title: 'Logo',
+        content: pageLoader(() => import('./DD-brand/brand-logo.md'))
+      },
+      {
+        path: '/DD-brand/color',
+        title: '顏色',
+        content: pageLoader(() => import('./DD-brand/brand-color.md'))
+      },
+    ]
+  },
+  {
+    title: "圖表",
+    pages: [
+      {
+        path: '/DD-chart/color',
+        title: '圖表用色',
+        content: pageLoader(() => import('./DD-Chart/chart-color.md'))
+      },
+      {
+        path: '/DD-chart/purpose',
+        title: '圖表用途',
+        content: pageLoader(() => import('./DD-Chart/chart-purpose.md'))
+      },
+    ]
   }
 ];
+const theme = {
+  fontFamily: ['Roboto, Helvetica, Arial, "黑體-繁", "Noto Sans TC", "微軟正黑體", sans-serif'],
+  pageHeadingBackground: '#616161',
+  textColor: '#595959',
+  linkColor: '#5CAE77',
+  sidebarColorText: '#494949',
+  sidebarColorTextActive: '#50AD56',
+  navBarTextColor: '#595959'
+}
+
+const customStyle = ['/dd.css']
+
 
 ReactDOM.render(
-  <Catalog title="Catalog" pages={pages} />,
-  document.getElementById("catalog")
+  <Catalog title="融數基地 DD Story Hub" 
+  pages={pages} 
+  theme={theme}
+  logoSrc={logo}
+  styles={customStyle}/>,
+  document.getElementById("dd-content")
 );
